ce2e23f5df5712d6ffcaa36cf79bfc166706f6fe
Anshul Bhilare <terminatoranshul@gmail.com>
2025-08-02 20:15:18 +0530

    Update toggle-group.tsx

--- src/components/ui/toggle-group.tsx
+++ src/components/ui/toggle-group.tsx
@@ -5,6 +5,7 @@ import { type VariantProps } from "class-variance-authority"
 import { cn } from "@/lib/utils"
 import { toggleVariants } from "@/components/ui/toggle"
 
+
 const ToggleGroupContext = React.createContext<
   VariantProps<typeof toggleVariants>
 >({
