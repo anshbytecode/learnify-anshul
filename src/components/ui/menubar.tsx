0ffc2fc61bef281c05641cc386d6e80dbea9a5dc
Anshul Bhilare <terminatoranshul@gmail.com>
2025-08-02 20:11:24 +0530

    Update menubar.tsx

--- src/components/ui/menubar.tsx
+++ src/components/ui/menubar.tsx
@@ -3,7 +3,6 @@ import * as MenubarPrimitive from "@radix-ui/react-menubar"
 import { Check, ChevronRight, Circle } from "lucide-react"
 
 import { cn } from "@/lib/utils"
-
 const MenubarMenu = MenubarPrimitive.Menu
 
 const MenubarGroup = MenubarPrimitive.Group
